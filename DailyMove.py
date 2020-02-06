ac98da9ee04c9f5c21e1002f83d50b760773cafe
Liam Li <ivy.liye@gmail.com>
2020-02-06 18:30:44 +0800

    updated

--- DailyMove.py
+++ DailyMove.py
@@ -3,18 +3,60 @@ import json
 import requests
 
 
-def getalldata():
 
-    token = "c7fa8e75250aed46fea546d486a8019cf0ee5a2af08b04d64786c9b04c6eaeac"
+def getalldata(td):
+
+    token = "75c383a6bba684f08359093d22146952c2ff1ee6f1369fb3f6f21c021bfe7674"
     headers = {"Authorization": "Bearer " + token}
     apiurl = 'https://api.wmcloud.com/data/v1//api/market/getMktEqud.json?'
-    param = {'beginDate': '', 'endDate': '', 'secID': '', 'ticker': '', 'tradeDate': '20190917','isOpen': ''}
+    param = {'beginDate': '', 'endDate': '', 'secID': '', 'ticker': '', 'tradeDate': td,'isOpen': ''}
     r=requests.get(apiurl, params=param, headers=headers)
 
     dataresult = json.loads(r.text)
 
-    print (json.dumps(dataresult,indent=2))
+    #the returned is a dictionary
+    #key
+    #retCode
+    #retMsg
+    #data
+        #data is a list[0-9999]
+
+            #list[0] is a dictionary
+
+                    # secID
+                    # ticker
+                    # secShortName
+                    # exchangeCD
+                    # tradeDate
+                    # preClosePrice
+                    # actPreClosePrice
+                    # openPrice
+                    # highestPrice
+                    # lowestPrice
+                    # closePrice
+                    # turnoverVol
+                    # turnoverValue
+                    # dealAmount
+                    # turnoverRate
+                    # accumAdjFactor
+                    # negMarketValue
+                    # marketValue
+                    # chgPct
+                    # PE
+                    # PE1
+                    # PB
+                    # isOpen
+                    # vwap
+
+    return dataresult
+
+
+    #print (json.dumps(dataresult,indent=2))
+
+
+
 
 if __name__ =='__main__':
-
-    getalldata()
+    d = getalldata('20200205')
+    for key in d['data'][0]:
+        print (key)
