46e443d3498289aa1ab2e8ae5b6313e3ad942d1b
Liam Li <ivy.liye@gmail.com>
2020-02-06 21:06:11 +0800

    updated

--- BlockTrade.py
+++ BlockTrade.py
@@ -6,7 +6,7 @@ def blocktrade(tradedate):
 
     tradedatestring = str(tradedate)
 
-    token = "c7fa8e75250aed46fea546d486a8019cf0ee5a2af08b04d64786c9b04c6eaeac"
+    token = "75c383a6bba684f08359093d22146952c2ff1ee6f1369fb3f6f21c021bfe7674"
     headers = {"Authorization": "Bearer " + token}
     apiurl = 'https://api.wmcloud.com/data/v1//api/market/getMktBlockd.json?'
     param = {'beginDate': '', 'endDate': '', 'secID': '', 'ticker': '', 'assetClass': '', 'tradeDate': tradedatestring}
--- DailyMove.py
+++ DailyMove.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import json
 import requests
+from datetime import date, timedelta
 
 
 
@@ -11,8 +12,14 @@ def getalldata(td):
     apiurl = 'https://api.wmcloud.com/data/v1//api/market/getMktEqud.json?'
     param = {'beginDate': '', 'endDate': '', 'secID': '', 'ticker': '', 'tradeDate': td,'isOpen': ''}
     r=requests.get(apiurl, params=param, headers=headers)
+    with open(str(td) + '.json', 'w') as file:
+        json.dump(r, file)
+
+
 
     dataresult = json.loads(r.text)
+
+
 
     #the returned is a dictionary
     #key
@@ -50,13 +57,21 @@ def getalldata(td):
 
     return dataresult
 
-
     #print (json.dumps(dataresult,indent=2))
 
 
 
 
 if __name__ =='__main__':
-    d = getalldata('20200205')
-    for key in d['data'][0]:
-        print (key)
+
+
+    #len(d['data'])=3873
+    #d[data][0][secId]
+    #print("\nCurrent Date: ", currentdate.strftime('%Y%m%d'))
+    #print("52 Weeks before current date: ", ftw.strftime('%Y%m%d'))
+    currentdate = date.today()-timedelta(days=1)
+    ftw= (date.today() - timedelta(weeks=52))
+    d = getalldata(currentdate)
+
+    for keys in d:
+        print (keys)
