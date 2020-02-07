b986ce98dc9e4e23f5da66f2247e8e4dd5e0f599
Liam Li <ivy.liye@gmail.com>
2020-02-07 09:28:43 +0800

    updated

--- DailyMove.py
+++ DailyMove.py
@@ -4,22 +4,17 @@ import requests
 from datetime import date, timedelta
 
 
-
 def getalldata(td):
 
     token = "75c383a6bba684f08359093d22146952c2ff1ee6f1369fb3f6f21c021bfe7674"
     headers = {"Authorization": "Bearer " + token}
     apiurl = 'https://api.wmcloud.com/data/v1//api/market/getMktEqud.json?'
-    param = {'beginDate': '', 'endDate': '', 'secID': '', 'ticker': '', 'tradeDate': td,'isOpen': ''}
+    param = {'beginDate': '', 'endDate': '', 'secID': '', 'ticker': '', 'tradeDate': td, 'isOpen': ''}
     r=requests.get(apiurl, params=param, headers=headers)
     with open(str(td) + '.json', 'w') as file:
         json.dump(r, file)
 
-
-
     dataresult = json.loads(r.text)
-
-
 
     #the returned is a dictionary
     #key
@@ -57,28 +52,28 @@ def getalldata(td):
 
     return dataresult
 
-    #print (json.dumps(dataresult,indent=2))
+    # print (json.dumps(dataresult,indent=2))
 
-def readlocaldata(data.json)
-    r=open(data)
+
+def readlocaldata(data):
+
+    r = open(data)
     dataresult = json.loads(r.text)
 
     return dataresult
 
 
-if __name__ =='__main__':
+if __name__ == "__main__":
 
+    # len(d['data'])=3873
 
-    #len(d['data'])=3873
-    #d[data][0][secId]
-    #print("\nCurrent Date: ", currentdate.strftime('%Y%m%d'))
-    #print("52 Weeks before current date: ", ftw.strftime('%Y%m%d'))
-
-
+    # d[data][0][secId]
+    # print("\nCurrent Date: ", currentdate.strftime('%Y%m%d'))
+    # print("52 Weeks before current date: ", ftw.strftime('%Y%m%d'))
 
     currentdate = date.today()-timedelta(days=1)
     ftw= (date.today() - timedelta(weeks=52))
     d = getalldata(currentdate)
 
     for keys in d:
-        print (keys)
+        print(keys)
