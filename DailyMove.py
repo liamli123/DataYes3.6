d0920c8e628e196de698fda2381a02185f20925c
Liam Li <ivy.liye@gmail.com>
2020-02-10 19:25:58 +0800

    updated

--- DailyMove.py
+++ DailyMove.py
@@ -2,6 +2,7 @@
 import json
 import requests
 from datetime import date, timedelta
+import pandas as pd
 
 
 def readalldata(td):
@@ -16,8 +17,6 @@ def readalldata(td):
 
     with open(str(td) + '.json', 'w') as file:
         json.dump(dataresult, file)
-
-    return dataresult
 
     #the returned is a dictionary
     #key
@@ -77,10 +76,12 @@ def findname(ticker, alldata):
 def findcloseprice(ticker, alldata):
     for i in alldata:
         if i['ticker'] == ticker:
+            # print(type(i['closePrice']))
+            # print(i['closePrice'])
             return i['closePrice']
 
 
-def gettickerdata(ticker, alldata):
+def gettickerdata(ticker, alldata): # display all info of a ticker
     for i in alldata:
         if i['ticker'] == ticker:
                 for keys in i:
@@ -88,9 +89,17 @@ def gettickerdata(ticker, alldata):
                     print(i[keys])
 
 def find52low(d1, d2):
+    df = pd.DataFrame(columns=('ticker', 'Name', '52week change'))
     for i in d1:
-        low = i['closePrice'] / findcloseprice(i['ticker'], d2)
-        print(low)
+        try:
+            newrow = {'ticker': i['ticker'], 'Name': findname(i['ticker'], d1), '52week change': (i['closePrice']/findcloseprice(i['ticker'], d2)-1)*100} #current price / 52 weeks ago
+            df = df.append(newrow, ignore_index=True)
+        except:
+            pass
+    df = df.sort_values(by='52week change', ascending=1)
+    return df
+
+        # print(i['closePrice'] / findcloseprice(i['ticker'], d2)
 
 
 if __name__ == '__main__':
@@ -100,20 +109,19 @@ if __name__ == '__main__':
     # print("\nCurrent Date: ", currentdate.strftime('%Y%m%d'))
     # print("52 Weeks before current date: ", ftw.strftime('%Y%m%d'))
 
-    currentdate = (date.today()-timedelta(days=2)).strftime('%Y%m%d')
+    currentdate = (date.today()-timedelta(days=0)).strftime('%Y%m%d')
     ftw = (date.today() - timedelta(weeks=52)).strftime('%Y%m%d')
 
-    #d = readalldata(ftw)
+    readalldata(currentdate)
+    readalldata(ftw)
 
-    d = readlocaldata('20200205.json')['data']
+    d1 = readlocaldata(currentdate + '.json')['data']
+    d2 = readlocaldata(ftw + '.json')['data']
 
     # for item in d['data']:
     #     print(item['closePrice']/findcloseprice(item['ticker'], d['data']))
     #     print(findname(item['ticker'], d['data']))
 
-    #gettickerdata
+    print(d1[0]['tradeDate'])
+    print(find52low(d1, d2))
 
-
-    #add result to dict, and sort dict
-    #parallel print
-    find52low(d, d)
