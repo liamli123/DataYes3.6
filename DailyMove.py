56a34c12533fe2bcb1ce1589b6faa2a7b05dd2be
Liam Li Mac Kisan <liye@kisansushi.cn>
2020-10-09 09:15:58 +0800

    updated 20201008

--- DailyMove.py
+++ DailyMove.py
@@ -8,59 +8,56 @@ import sys
 
 
 def readalldata(td):
-
     token = "f5e83593972fd7bd68b6891076decb1b0dc1693f34b891e81a6e5b9ab937fa2e"
     headers = {"Authorization": "Bearer " + token}
     apiurl = 'https://api.wmcloud.com/data/v1//api/market/getMktEqud.json?'
     param = {'beginDate': '', 'endDate': '', 'secID': '', 'ticker': '', 'tradeDate': td, 'isOpen': ''}
-    r=requests.get(apiurl, params=param, headers=headers)
+    r = requests.get(apiurl, params=param, headers=headers)
 
     dataresult = json.loads(r.text)
 
     with open(str(td) + '.json', 'w') as file:
         json.dump(dataresult, file)
 
-    #the returned is a dictionary
-    #key
-    #retCode
-    #retMsg
-    #data
-        #data is a list[0-9999]
+    # the returned is a dictionary
+    # key
+    # retCode
+    # retMsg
+    # data
+    # data is a list[0-9999]
 
-            #list[0] is a dictionary
+    # list[0] is a dictionary
 
-                    # secID
-                    # ticker
-                    # secShortName
-                    # exchangeCD
-                    # tradeDate
-                    # preClosePrice
-                    # actPreClosePrice
-                    # openPrice
-                    # highestPrice
-                    # lowestPrice
-                    # closePrice
-                    # turnoverVol
-                    # turnoverValue
-                    # dealAmount
-                    # turnoverRate
-                    # accumAdjFactor
-                    # negMarketValue
-                    # marketValue
-                    # chgPct
-                    # PE
-                    # PE1
-                    # PB
-                    # isOpen
-                    # vwap
+    # secID
+    # ticker
+    # secShortName
+    # exchangeCD
+    # tradeDate
+    # preClosePrice
+    # actPreClosePrice
+    # openPrice
+    # highestPrice
+    # lowestPrice
+    # closePrice
+    # turnoverVol
+    # turnoverValue
+    # dealAmount
+    # turnoverRate
+    # accumAdjFactor
+    # negMarketValue
+    # marketValue
+    # chgPct
+    # PE
+    # PE1
+    # PB
+    # isOpen
+    # vwap
 
     # print (json.dumps(dataresult,indent=2))
 
 
 def readlocaldata(data):
-
     with open(data, 'r') as myfile:
-
         d = myfile.read()
 
     dataresult = json.loads(d)
@@ -74,7 +71,6 @@ def findname(ticker, alldata):
             return i['secShortName']
 
 
-
 def findcloseprice(ticker, alldata):
     for i in alldata:
         if i['ticker'] == ticker:
@@ -83,16 +79,15 @@ def findcloseprice(ticker, alldata):
             return i['closePrice']
 
 
-def gettickerdata(ticker, alldata): # display all info of a ticker
+def gettickerdata(ticker, alldata):  # display all info of a ticker
     for i in alldata:
         if i['ticker'] == ticker:
-                for keys in i:
-                    print(keys)
-                    print(i[keys])
+            for keys in i:
+                print(keys)
+                print(i[keys])
 
 
 def find52low(td):
-
     currentdate = datetime.strptime(td, '%Y%m%d').strftime('%Y%m%d')
 
     ftw = (datetime.strptime(td, '%Y%m%d') - timedelta(weeks=52)).strftime('%Y%m%d')
@@ -107,9 +102,10 @@ def find52low(td):
     for i in d1:
         try:
             newrow = {'ticker': i['ticker'], 'Name': findname(i['ticker'], d1),
-                      '52week change': (i['closePrice']/findcloseprice(i['ticker'], d2)-1)*100,
+                      '52week change': (i['closePrice'] / findcloseprice(i['ticker'], d2) - 1) * 100,
                       'TradeDate Close Price': i['closePrice'],
-                      '          PE': i['PE']}
+                      '          PE': i['PE'],
+                      '          Exchange': i['exchangeCD']}
             df = df.append(newrow, ignore_index=True)
         except:
             pass
@@ -119,7 +115,6 @@ def find52low(td):
 
 
 def backtest(tradedate, testdate):
-
     currentdate = datetime.strptime(tradedate, '%Y%m%d').strftime('%Y%m%d')
 
     ftw = (datetime.strptime(tradedate, '%Y%m%d') - timedelta(weeks=52)).strftime('%Y%m%d')
@@ -143,15 +138,16 @@ def backtest(tradedate, testdate):
     j = 0
     while j < allrows:
         try:
-            df.loc[j, testdate+' Price'] = findcloseprice(df.loc[j, 'ticker'], d3)
-            df.loc[j, testdate+' Change'] = (df.loc[j, testdate+' Price']/df.loc[j, 'TradeDate Close Price']-1)*100
+            df.loc[j, testdate + ' Price'] = findcloseprice(df.loc[j, 'ticker'], d3)
+            df.loc[j, testdate + ' Change'] = (df.loc[j, testdate + ' Price'] / df.loc[
+                j, 'TradeDate Close Price'] - 1) * 100
         except:
             pass
-        j = j+1
+        j = j + 1
 
     print('Trade Date:  ' + d1[0]['tradeDate'])
 
-    df = df.sort_values(by=testdate+' Change', ascending=0)
+    df = df.sort_values(by=testdate + ' Change', ascending=0)
 
     with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.width', None,
                            'display.colheader_justify', 'left'):  # more options can be specified also
@@ -163,16 +159,16 @@ if __name__ == '__main__':
     # backtest('20170712', '20190813')
 
     with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.width', None,):
-        print(find52low('20200221'))
+         print(find52low('20200831'))
 
 
-    # readalldata to load two range of data
+# readalldata to load two range of data
 
-    # find52low(finddate) - return dataframe
+# find52low(finddate) - return dataframe
 
-    # backtest(finddate, test date) - print dataframe
+# backtest(finddate, test date) - print dataframe
 
-    # print(find52low('20190211'))
+# print(find52low('20190211'))
 
-    # d = readlocaldata('20200210.json')['data']
-    # print(findcloseprice('300304', d))
+# d = readlocaldata('20200210.json')['data']
+# print(findcloseprice('300304', d))
